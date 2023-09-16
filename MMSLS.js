7cc79f2f27e24c3ab9967134abbbd332782fd235
HRBX2001 <141107826+HRBX2001@users.noreply.github.com>
2023-09-17 03:25:53 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=14764880&pid=e539b29734b499bd2e69b131b627419d&tid=36b04677b63316e243ee3b2ffa10d551",
+    "https://www.maimemo.com/share/page?uid=14764880&pid=2ed62722f344fb720c09b790631981f6&tid=2da89503e9e95e7a584da68053ccbe3a",
 
   ]
   /**
